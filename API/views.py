fae74b982c8fe38c103deda9e9e9f55218749cd7
Jhordanex <jhordan.sanchez@tecsup.edu.pe>
2024-06-19 15:46:00 -0500

    Subiendo api para paypal

--- API/views.py
+++ API/views.py
@@ -88,7 +88,7 @@ def comprar_curso(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
+""" @api_view(['POST'])
 def crear_venta(request):
     plan_id = request.data.get('plan_id')
     alumno_id = request.user.id  # Obtener el ID del usuario autenticado
@@ -103,16 +103,19 @@ def crear_venta(request):
     )
 
     return Response({'venta_id': venta.id}, status=status.HTTP_201_CREATED) 
-
+ """
 @api_view(['POST'])
 def save_payment(request):
     try:
         monto = float(request.data.get('monto'))
         fecha_registro = request.data.get('fecha_registro')
-        venta_id = request.data.get('venta_id')
 
-        # Obtener la instancia de Venta correspondiente
-        venta = Venta.objects.get(id=123)
+        # Crear una nueva instancia de Venta
+        venta = Venta.objects.create(
+            monto=monto,
+            fecha_registro=fecha_registro,
+            # otros campos necesarios para crear una Venta
+        )
 
         # Guardar el pago en el modelo VentaPago
         pago = VentaPago.objects.create(
@@ -138,14 +141,14 @@ def save_payment(request):
     except Exception as e:
         return Response({'error': str(e)}, status=400)
 
-# Configuración de PayPal
+""" # Configuración de PayPal
 client_id = settings.PAYPAL_CLIENT_ID
 client_secret = settings.PAYPAL_CLIENT_SECRET
 
 environment = SandboxEnvironment(client_id=client_id, client_secret=client_secret)
-client = PayPalHttpClient(environment)
+client = PayPalHttpClient(environment) """
 
-@api_view(['POST'])
+""" @api_view(['POST'])
 def iniciar_pago_paypal(request):
     try:
         # Obtener el ID del plan y el ID de usuario del cuerpo de la solicitud
@@ -208,7 +211,7 @@ def confirmar_pago_paypal(request):
             return Response({'error': 'No se pudo completar el pago en PayPal'}, status=status.HTTP_400_BAD_REQUEST)
 
     except Venta.DoesNotExist:
-        return Response({'error': 'Venta no encontrada'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'Venta no encontrada'}, status=status.HTTP_404_NOT_FOUND) """
 
 @api_view(['GET'])
 def curso_detail(request):
