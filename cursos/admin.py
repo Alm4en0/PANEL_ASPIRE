4d9b82d10027385f58bf48ff4b9000c0a60840dd
Alm4en0 <almendra.garcia@tecsup.edu.pe>
2024-06-21 15:17:44 -0500

    confirmar merge

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import CategoriaCurso, SubCategoriaCurso, Curso, Plan, Venta, PlanCurso, InscripcionCurso, VentaCurso, ModuloCurso, RecursoCurso, VentaPago, RegistroLanding, VentaPaypal
+from .models import CategoriaCurso, SubCategoriaCurso, Curso, Plan, PlanCurso, InscripcionCurso, ModuloCurso, RecursoCurso, RegistroLanding, VentaPaypal
 
 class CategoriaCursoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'descripcion', 'estado', 'codigo', 'imagen_categoria')
@@ -41,7 +41,7 @@ class PlanAdmin(admin.ModelAdmin):
     search_fields = ('nombre',)
     list_filter = ('estado',)
 
-class VentaAdmin(admin.ModelAdmin):
+""" class VentaAdmin(admin.ModelAdmin):
     list_display = ('id', 'alumno', 'fecha_venta_formatted', 'monto')
     search_fields = ('alumno__username',)
     list_filter = ('fecha_venta',)
@@ -50,7 +50,7 @@ class VentaAdmin(admin.ModelAdmin):
     def fecha_venta_formatted(self, obj):
         if obj.fecha_venta:
             return obj.fecha_venta.strftime("%d-%m-%Y %H:%M:%S")
-        return '-'
+        return '-' """
 
 class PlanCursoAdmin(admin.ModelAdmin):
     list_display = ('id', 'plan', 'curso')
@@ -68,7 +68,7 @@ class InscripcionCursoAdmin(admin.ModelAdmin):
             return obj.fecha_registro.strftime("%d-%m-%Y %H:%M:%S")
         return '-'
 
-class VentaCursoAdmin(admin.ModelAdmin):
+""" class VentaCursoAdmin(admin.ModelAdmin):
     list_display = ('id', 'venta', 'plan_curso', 'cantidad', 'fecha_venta_formatted')
     search_fields = ( 'curso__nombre',)
     list_filter = ('fecha_venta',)
@@ -77,7 +77,7 @@ class VentaCursoAdmin(admin.ModelAdmin):
     def fecha_venta_formatted(self, obj):
         if obj.fecha_venta:
             return obj.fecha_venta.strftime("%d-%m-%Y %H:%M:%S")
-        return '-'
+        return '-' """
 
 class ModuloCursoAdmin(admin.ModelAdmin):
     list_display = ('id', 'curso', 'nombre', 'descripcion','estado', 'link', 'duracion')
@@ -89,7 +89,7 @@ class RecursoCursoAdmin(admin.ModelAdmin):
     search_fields = ('curso__nombre', 'descripcion', 'nombre_recurso', 'url_recurso')
     list_filter = ('curso',)
 
-class VentaPagoAdmin(admin.ModelAdmin):
+""" class VentaPagoAdmin(admin.ModelAdmin):
     list_display = ('id', 'venta', 'monto', 'fecha_registro_formatted')
     search_fields = ('venta__plan__nombre',)
     list_filter = ('fecha_registro',)
@@ -98,7 +98,7 @@ class VentaPagoAdmin(admin.ModelAdmin):
     def fecha_registro_formatted(self, obj):
         if obj.fecha_registro:
             return obj.fecha_registro.strftime("%d-%m-%Y %H:%M:%S")
-        return '-'
+        return '-' """
 
 class RegistroLandingAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'apellido', 'correo', 'celular', 'fecha_registro_formatted')
@@ -127,11 +127,9 @@ admin.site.register(CategoriaCurso, CategoriaCursoAdmin)
 admin.site.register(SubCategoriaCurso, SubCategoriaCursoAdmin)
 admin.site.register(Curso, CursoAdmin)
 admin.site.register(Plan, PlanAdmin)
-admin.site.register(Venta, VentaAdmin)
+
 admin.site.register(PlanCurso, PlanCursoAdmin)
 admin.site.register(InscripcionCurso, InscripcionCursoAdmin)
-admin.site.register(VentaCurso, VentaCursoAdmin)
 admin.site.register(ModuloCurso, ModuloCursoAdmin)
 admin.site.register(RecursoCurso, RecursoCursoAdmin)
-admin.site.register(VentaPago, VentaPagoAdmin)
 admin.site.register(RegistroLanding, RegistroLandingAdmin)
